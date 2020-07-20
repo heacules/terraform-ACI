2e76843b854096ba3f599bdf3aa4d0b44b3c5d39
David Nyring Pedersen <dany@tv2.dk>
2020-07-20 10:09:37 +0200

    first try

---
+++ terrafor_test01.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+ provider "aci" {
+    #required_version = "= 0.3.3"
+    # cisco-aci user name
+    username = "admin"
+    # cisco-aci password
+    password = "ciscopsdt"
+    # cisco-aci url
+    url      = "https://sandboxapicdc.cisco.com"
+    insecure = true
+}
+
+resource "aci_tenant" "terraform-rocks_2"{
+   name = "terraform-rocks_2"
+   description ="This tenant is created by terraform"
+}
+
+resource "aci_vrf" "vrf_a"{
+   tenant_dn = aci_tenant.terraform-rocks_2.id
+   name = "VRF_A_name"  
+}
+
+data "aci_tenant" "import_2"{
+  name = "import_test"
+}
+
+
+#resource "aci_tenant" "test-tenant" {
+#  name        = "test-tenant"
+#  description = "This tenant is created by terraform"
+#}
+
+resource "aci_application_profile" "test-app" {
+  tenant_dn   = aci_tenant.terraform-rocks_2.id
+  name        = "test-app"
+  description = "This app profile is created by terraform"
+}
+
+resource "aci_bridge_domain" "terraform_bridge"{
+    tenant_dn = aci_tenant.terraform-rocks_2.id
+    name = "Terraform_bridge"
+    arp_flood = "yes"
+    relation_fv_rs_ctx = aci_vrf.vrf_a.id
+}
+
+#resource "aci_subnet" "terraformsubnet" {
+#    bridge_domain_dn = aci_bridge_domain.terraform_bridge.id
+#    ip = "10.60.23.0/24"
+#    #scope = "private"
+#}
+resource "aci_application_epg" "terraform_epg"{
+    application_profile_dn = aci_application_profile.test-app.id
+    name = "terraform_epg"
+}
