4476f5c39a57baead0e158f724ddb3cb2a7cb655
David Nyring Pedersen <dany@tv2.dk>
2020-07-20 13:34:15 +0200

    local varibals works

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+#variable "vrf_name" {
+#    type = string 
+#    description = "et sted at kalde VRF"
+#    default = ["VRF_B"]
+#    }
+#    
--- terraform_test01.tf
+++ terraform_test01.tf
@@ -2,6 +2,14 @@ terraform {
   required_version = ">= 0.12"
 }
 
+locals {
+ #vrf = "vrf_a"
+  vrf = {
+    1 = "VRF_A"
+    2 = "VRF_B"
+  }
+}
+
  provider "aci" {
     #required_version = "= 0.3.3"
     # cisco-aci user name
@@ -20,13 +28,8 @@ resource "aci_tenant" "terraform-rocks_2"{
 
 resource "aci_vrf" "vrf_a"{
    tenant_dn = aci_tenant.terraform-rocks_2.id
-   name = "VRF_A_name"  
+   name = local.vrf.1
 }
-
-data "aci_tenant" "import_2"{
-  name = "import_test"
-}
-
 
 #resource "aci_tenant" "test-tenant" {
 #  name        = "test-tenant"
@@ -46,12 +49,20 @@ resource "aci_bridge_domain" "terraform_bridge"{
     relation_fv_rs_ctx = aci_vrf.vrf_a.id
 }
 
-#resource "aci_subnet" "terraformsubnet" {
-#    bridge_domain_dn = aci_bridge_domain.terraform_bridge.id
-#    ip = "10.60.23.0/24"
-#    #scope = "private"
-#}
+resource "aci_subnet" "terraformsubnet" {
+    bridge_domain_dn = aci_bridge_domain.terraform_bridge.id
+    ip = "10.60.23.1/24"
+    #scope = "private"
+}
 resource "aci_application_epg" "terraform_epg"{
     application_profile_dn = aci_application_profile.test-app.id
     name = "terraform_epg"
 }
+#
+#data "aci_tenant" "import_2"{
+#  name = "import_test"
+#}
+#resource "aci_vrf" "vrf_b"{
+#   tenant_dn = aci_tenant.import_test.id
+#   name = locals.vrf.2  
+#}
